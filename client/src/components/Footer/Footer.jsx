104263ecb6c70d9f2327588b6583827b389f2a05
Alfredo Valente Langa <alfredolanga360@gmail.com>
2024-12-17 11:51:11 +0100

    Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
 function Footer() {
   return (
     <div className="bg-slate-900 text-white w-full py-10">
       <footer className="flex flex-wrap justify-between px-10 lg:px-40">
      
+        {/* Seção 1 - Contacto */}
         <div className="w-full md:w-1/3 mb-8 md:mb-0">
           <h2 className="text-xl font-bold mb-4">Contacte-nos</h2>
           <ul className="space-y-2">
@@ -14,7 +15,7 @@ function Footer() {
           </ul>
         </div>
 
-       
+        {/* Seção 2 - Links rápidos */}
         <div className="w-full md:w-1/3 mb-8 md:mb-0">
           <h2 className="text-xl font-bold mb-4">Links Rápidos</h2>
           <ul className="space-y-2">
@@ -24,24 +25,34 @@ function Footer() {
           </ul>
         </div>
 
-        {/* Seção 3 */}
+        {/* Seção 3 - Redes sociais */}
         <div className="w-full md:w-1/3">
-          <h2 className="text-xl font-bold mb-4">Siga-nos</h2>
-          <ul className="flex space-x-4">
+          <h2 className="text-xl font-bold mb-4">Fale connosco</h2>
+          <ul className="flex flex-col gap-4">
             <li>
-              <a href="#" className="hover:text-blue-400">Facebook</a>
+              <a href="#" className="hover:text-blue-400 flex items-center">
+                <FaFacebookF className="mr-2" />
+                Facebook
+              </a>
             </li>
             <li>
-              <a href="#" className="hover:text-blue-400">Instagram</a>
+              <a href="#" className="hover:text-blue-400 flex items-center">
+                <FaInstagram className="mr-2" />
+                Instagram
+              </a>
             </li>
             <li>
-              <a href="#" className="hover:text-blue-400">LinkedIn</a>
+              <a href="#" className="hover:text-blue-400 flex items-center">
+                <FaLinkedinIn className="mr-2" />
+                LinkedIn
+              </a>
             </li>
           </ul>
         </div>
+
       </footer>
 
-     
+      {/* Rodapé Final */}
       <div className="text-center mt-8 border-t border-gray-700 pt-4">
         <p className="text-gray-400">&copy; {new Date().getFullYear()} Alfredo Langa.</p>
       </div>
